fc2832aefc3646f978c90858e8d6bd1d858f0482
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-23 16:29:36 +0200

    feat:add solution

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
 // solution_1
-const combine = (...params) => params.reduce((a, b) => {
-  for (const x in b) { a[x] = x in a ? a[x] + b[x] : b[x] };
-  return a;
- }, {});
-  
-// solution_2
+function nbDig(n, d) {
+  let arr=[]
+  let countTheDigit=0
+  for(let k=0; k<=n;k++){arr.push(k**2)}
+  console.log(arr)
+  let arrDigit =[]
+  arr.forEach(el => {
+    arrDigit = el.toString().split('')
+    console.log(arrDigit)
+    for(let i=0; i<=arrDigit.length;i++){
+      if(arrDigit[i]==d){
+         countTheDigit=countTheDigit+1
+      }
+    }
 
-const combine = (...args) =>
-  args.reduce((pre, val) => (Object.keys(val).forEach(v => pre[v] = (pre[v] || 0) + val[v]), pre), {});
+})
+return countTheDigit 
+}
+// solution_2
+function nbDig(n, d) {
+  let arr = [];
+
+  for (let i = 0; i <= n; i++) {
+      arr.push(Math.pow(i, 2));
+  }
+
+  
+  return arr.join('').split('').map(x => x.includes(d)).reduce((a, b) => a + b, 0);
+
+}
 
 
   // solution_3
 
-  function combine() {
-    var obj = {}
-  
-    for (var i = 0; i < arguments.length; i++) {
-          for (var key in arguments[i]) {
-            obj[key] = obj[key] ? obj[key] + arguments[i][key]: arguments[i][key]
-          }
-    }
-  
-    return obj;
-  }
+  let nbDig = (n,d) => [...Array(++n)].reduce((Σ,_,i)=> (`${i*i}`.match(RegExp(d,'g')) || []).length + Σ, 0)
+
   // solution_4
 
-  const combine = (...o) => (b=>{
-    for(var k in o) for(var i in o[k])
-      b[i] = (i in b) ? b[i] + o[k][i] : o[k][i]; return b;})({});
+  function nbDig(n, d) {
+    // your code
+  let arr = Array(n+1).fill(1).map((el,i) => i**2).join('').split('').filter(el=> el == d);
+
+  return arr.length;
+}
 
   // solution_5
-
-  const combine = (...xs) => xs.reduce((a, b) => {
-    const result = {...a}
-    for (const prop in b) {
-      result[prop] = b[prop] + (result[prop] ? result[prop] : 0)
-    }
-  
-    return result
-  }, {})
+  console.log(`countTheDigit= ${nbDig(25,1)}`)
